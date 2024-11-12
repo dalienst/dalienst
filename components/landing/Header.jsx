da1401913ea740ed0877095ec2940a1f9efcac8e
dalienst <oduordalienst@gmail.com>
2024-11-12 13:09:52 +0300

    modified header

--- components/landing/Header.jsx
+++ components/landing/Header.jsx
@@ -9,14 +9,14 @@ function Header() {
         <div className="container">
           <h1 className="text-center header-title">Dalienst Owino Oduor</h1>
 
-          <p className="text-center header-description mt-3">
+          <p className="text-center header-description mt-4">
             Offering strategic business consulting, digital transformation
             expertise, and technology solutions to drive growth, efficiency, and
             innovation. Integrating technology and modern software to empower
             your business.
           </p>
 
-          <div className="d-flex gap-3 mt-3 justify-content-center">
+          <div className="d-flex gap-3 mt-4 justify-content-center">
             <Link href="#" className="primary-btn btn btn-sm rounded">
               Get In Touch
               <span className="ms-2">
