f48808328048abcdc730d08d509477a8072dfebd
dalienst <oduordalienst@gmail.com>
2025-05-21 09:15:48 +0300

    fix language

--- components/landing/Services.jsx
+++ components/landing/Services.jsx
@@ -8,7 +8,7 @@ function Services() {
       <div className="py-5 services-bg px-4 text-white rounded-3">
         <section className="mb-3 d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center">
           <div>
-            <h1>My Service</h1>
+            <h1>My Services</h1>
             <p>
               <small>
                 Technology oriented solutions to boost your business.
@@ -44,12 +44,7 @@ function Services() {
                       <small>{service?.description}</small>
                     </p>
 
-                    {/* <Link href="#" className="btn btn-sm btn-outline-light">
-                      Schedule Discussion{" "}
-                      <span>
-                        <i className="bi bi-chevron-right" />
-                      </span>
-                    </Link> */}
+                    
                   </div>
                 </div>
               </div>
